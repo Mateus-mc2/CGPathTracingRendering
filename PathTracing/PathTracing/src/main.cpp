5943728a86c83e80a4495b300b1d9b05f5dbcd8e
mateus.leite <mateus.leite@gprt.ufpe.br>
2015-12-21 16:27:39 -0300

    Alterando a cagada no .gitignore que eu fiz...

---
+++ PathTracing/PathTracing/src/main.cpp
@@ -0,0 +1,20 @@
+#include <Eigen\Dense>
+#include <opencv2\core\core.hpp>
+#include <opencv2\imgproc\imgproc.hpp>
+#include <opencv2\highgui\highgui.hpp>
+
+#include <iostream>
+
+using Eigen::MatrixXi;
+
+int main() {
+  MatrixXi I = MatrixXi::Identity(480, 480);
+  I = 255*I;
+  MatrixXi transposta = I.transpose();
+  cv::Mat input(I.rows(), I.cols(), CV_8UC1, transposta.data());
+
+  std::cout << "Bunda." << std::endl;
+  cv::imshow("Teste", input);
+  cv::waitKey(0);
+  return 0;
+}
