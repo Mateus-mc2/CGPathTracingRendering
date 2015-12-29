f333f02ae2346cbc5bff0d37d3a37aeebea9ba9a
Rodrigo F. Figueiredo <rodrigo.figueiredo@gprt.ufpe.br>
2015-12-29 17:34:43 -0300

    Inicio da implementação da etapa de 'Inicialização' onde será lido o arquivo de texto no formato SDL, e a partir deste, criar todas as instâncias de objetos necessários para os cálculos da renderização propriamente dita na etapa de 'Processamento'.

--- PathTracing/PathTracing/include/camera.h
+++ PathTracing/PathTracing/include/camera.h
@@ -8,11 +8,24 @@ namespace util {
 typedef Eigen::Matrix<double, 3, 4> CameraMatrix;
 
 struct Camera {
-  Camera(const Eigen::Vector3d &eye, const Eigen::Vector2d &bottom,
-         const Eigen::Vector2d &top, const double width, const double height);
+  Camera(const Eigen::Vector3d &eye,
+         const Eigen::Vector2d &bottom,
+         const Eigen::Vector2d &top,
+         const double width,
+         const double height)
+      : eye_(eye),
+        bottom_(bottom),
+        top_(top),
+        width_(width),
+        height_(height) {}
+
   ~Camera() {}
 
-  CameraMatrix camera_matrix_;
+  const Eigen::Vector3d eye_;
+  const Eigen::Vector2d bottom_;
+  const Eigen::Vector2d top_;
+  const double width_;
+  const double height_;
 };
 
 }  // namespace util
